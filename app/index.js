027b2681abb7c6e47433cfec0cfbbf6cf42a3abe
Carlos Costa ADM <8918adm@tce.pe>
2021-10-04 10:38:37 -0300

    adicionando banco de dados

--- app/index.js
+++ app/index.js
@@ -1,6 +1,33 @@
-const express = require('express');
-const app = express();app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const express  = require('express')
+const app = express();
+const port = 5000;
 
-app.listen(5000, () => console.log('Server is up and running'));
+const config = {
+    host: 'mysqldb',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+}
+
+const mysql = require('mysql');
+const connection = mysql.createConnection(config);
+const name;
+
+const sql = `INSERT INTO people(name) VALUES('Maya')`;
+connection.query(sql);
+
+const sql_select = `SELECT * FROM people`;
+connection.query(sql_select, function(err, result, fields) {
+  name = result;
+});
+
+connection.end();
+
+
+app.get('/', (req, res) => {
+    res.send('<h1>Maya</h1><br /> <h2>' + name + '</h2>')
+});
+
+app.listen(port, () => {
+    console.log('Rodando na porta ', port);
+})
