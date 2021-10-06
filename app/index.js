81f504fed32a79adb1a2cc2e958ea681e55c5665
brhujahster <c.diego.f.costa@gmail.com>
2021-10-06 17:00:30 -0300

    testando

--- app/index.js
+++ app/index.js
@@ -2,30 +2,30 @@ const express  = require('express')
 const app = express();
 const port = 5000;
 
-const config = {
-    host: 'mysqldb',
-    user: 'root',
-    password: 'root',
-    database: 'nodedb'
-}
+// const config = {
+//     host: 'mysqldb',
+//     user: 'root',
+//     password: 'root',
+//     database: 'nodedb'
+// }
 
-const mysql = require('mysql');
-const connection = mysql.createConnection(config);
-const name;
+// const mysql = require('mysql');
+// const connection = mysql.createConnection(config);
+// const name;
 
-const sql = `INSERT INTO people(name) VALUES('Maya')`;
-connection.query(sql);
+// const sql = `INSERT INTO people(name) VALUES('Maya')`;
+// connection.query(sql);
 
-const sql_select = `SELECT * FROM people`;
-connection.query(sql_select, function(err, result, fields) {
-  name = result;
-});
+// const sql_select = `SELECT * FROM people`;
+// connection.query(sql_select, function(err, result, fields) {
+//   name = result;
+// });
 
-connection.end();
+// connection.end();
 
 
 app.get('/', (req, res) => {
-    res.send('<h1>Maya</h1><br /> <h2>' + name + '</h2>')
+    res.send('<h1>Maya</h1><br />')
 });
 
 app.listen(port, () => {
